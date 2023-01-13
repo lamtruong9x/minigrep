d5f1343beb78c91062bd7803e85411b6eaaa5ee7
lamtruong <damlamtruong@gmail.com>
2023-01-13 21:38:43 +0700

    modulize our code for TDD

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{env, fs};
 use std::process;
 use std::error::Error;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,35 +13,9 @@ fn main() {
 
     // read file
     println!("In file {}", conf.file_path);
-    if let Err(e) = run(conf) {
+    if let Err(e) = minigrep::run(conf) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-
-}
-
-fn run(conf: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(conf.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
