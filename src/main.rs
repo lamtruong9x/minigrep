bedef08476b34b723daedcf1682f18b72d136a56
lamtruong <damlamtruong@gmail.com>
2023-01-14 10:38:48 +0700

    print error into stderr

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let conf = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // read file
     println!("In file {}", conf.file_path);
     if let Err(e) = minigrep::run(conf) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
